e41f9b9fd5bc6bc4b9872e4e4d794da1aaf3ab56
Patryk Karasiński <patrykkarasinski11@gmail.com>
2021-07-28 00:25:25 +0200

    charge variables

---
+++ script.js
@@ -0,0 +1,35 @@
+// Memory buttons
+const mc = document.querySelector('.mc');
+const mr = document.querySelector('.mr');
+const mPlus = document.querySelector('.m-plus');
+const mMinus = document.querySelector('.m-minus');
+const ms = document.querySelector('.ms');
+const showMemory = document.querySelector('.show-memory');
+
+// Figure buttons
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+
+// Sign buttons
+const percent = document.querySelector('.percent');
+const clearEntry = document.querySelector('.ce');
+const clear = document.querySelector('.c');
+const undo = document.querySelector('.undo');
+const reciprocal = document.querySelector('.reciprocal');
+const escalate = document.querySelector('.escalate');
+const squareRoot = document.querySelector('.square-root');
+const divide = document.querySelector('.divide');
+const multiply = document.querySelector('.multiply');
+const subtract = document.querySelector('.subtract');
+const add = document.querySelector('.add');
+const plusOrMinus = document.querySelector('.plus-or-minus');
+const comma = document.querySelector('.comma');
+const equal = document.querySelector('.equal');
